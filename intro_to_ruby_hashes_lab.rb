3a1610bd1c182c9e196dd455418a2e7fb73d0a90
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-19 17:50:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,9 +17,9 @@ def monopoly_with_second_tier
  }
  
  monopoly[:railroads][] = {
-  pieces: 4,
-  rent_in_dollars: {},
-  names: {}
+  :pieces => 4,
+  :rent_in_dollars {},
+  :names: {}
  }
  
  monopoly
